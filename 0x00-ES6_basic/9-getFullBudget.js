f983a87594400e6219b287b86566e28c7c235cbb
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-28 21:57:32 -0500

    [style]: semicolon added

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,6 +2,7 @@ import getBudgetObject from './7-getBudgetObject.js'
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
+
   const fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
@@ -10,7 +11,7 @@ export default function getFullBudgetObject(income, gdp, capita) {
     getIncomeInEuros(income) {
       return `${income} euros`;
     },
-  }
+  };
 
   return fullBudget;
 }
