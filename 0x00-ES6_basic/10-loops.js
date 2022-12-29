7e7bdf10f3120b7717d7b72d7591a9d651fb821e
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-29 01:50:29 -0500

    [fix]: used a copy of array by value

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const newArray = array;
-  for (let [idx, value] of array.entries()) {
+  const newArray = [...array];
+  for (const [idx, value] of newArray.entries()) {
     newArray[idx] = appendString + value;
   }
 
-  return array;
+  return newArray;
 }
