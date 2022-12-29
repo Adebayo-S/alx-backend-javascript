2593a005dec28f09812030bc1c740c2939b86d5c
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-29 01:51:35 -0500

    [fix]: used sime for --of loop

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,16 @@
-export default function appendToEachArrayValue(array, appendString) {
-  const newArray = [...array];
-  for (const [idx, value] of newArray.entries()) {
-    newArray[idx] = appendString + value;
-  }
+// export default function appendToEachArrayValue(array, appendString) {
+//   const newArray = [...array];
+//   for (const [idx, value] of newArray.entries()) {
+//     newArray[idx] = appendString + value;
+//   }
 
-  return newArray;
+//   return newArray;
+// }
+
+export default function appendToEachArrayValue(array, appendString) {
+  const res = [];
+  for (const element of array) {
+    res.push(`${appendString}${element}`);
+  }
+  return res;
 }
