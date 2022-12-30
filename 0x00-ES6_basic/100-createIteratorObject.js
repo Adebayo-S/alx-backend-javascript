f2546c577fa8f70e513d00eadf6158bf39f1f588
Adebayo Samuel <eladebayoor@gmail.com>
2022-12-29 21:41:37 -0500

    [feat]: file 100 added

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,9 +1,7 @@
 export default function createIteratorObject(report) {
   let report = [];
-  for (let val of Object.values(report)) {
-    for (let employee of Object.values(val)) {
-      report.push(...employee);
-    }
+  for (let val of Object.values(report.allEmployees)) {
+    report = [...report, ...val];
   }
   return report;
 }
