08fc4efa60ecf6195cc7fbfabbc5c3049b3f9b49
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-03 02:05:52 -0500

    [feat]: file 1 added

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,19 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise(function (resolve, reject) {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success'
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  // }).then(() => {
+  //   return {
+  //     status: 200,
+  //     body: 'Success'
+  //   }
+  // }).catch(() => {
+
+  // });
+})}
