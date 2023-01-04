410ef04f8bac71d3c6cf6f7f2987ab3b3a4c28f7
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-03 22:13:54 -0500

    [style]: eslint requirements

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
     return { photo, user };
-  } catch {
-    return { photo: null, user: null }
+  } catch (err) {
+    return { photo: null, user: null };
   }
 }
