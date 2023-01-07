c58199d06098183bd6f85c617e90a47da8122e72
Adebayo Samuel <eladebayoor@gmail.com>
2023-01-06 23:42:44 -0500

    [feat]: student class added

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -14,3 +14,30 @@ interface Directors extends Teacher {
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName[0]}. ${lastName}`
 }
+
+interface StudentClassConstructor {
+  new(firstName: string, lastName: string): Blueprint;
+}
+
+interface Blueprint {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements Blueprint {
+  private _firstName: string;
+  private _lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working'
+  }
+
+  displayName(): string {
+    return this._firstName
+  }
+}
